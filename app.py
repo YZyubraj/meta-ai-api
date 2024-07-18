365df825aa4dd516ae11b2024c62860800339743
YZyubraj <94026600+YZyubraj@users.noreply.github.com>
2024-07-18 09:13:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from src.meta_ai_api.main import MetaAI
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
+@app.route('/prompt/<message>')
+def prompt(message):
+    meta = MetaAI()
+    response = meta.prompt(message, stream=False)
+    return jsonify(response)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
